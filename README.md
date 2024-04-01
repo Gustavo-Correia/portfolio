b1ccac58ae95b785db8117fd34328b3b5069bb01
Gustavo Correia <gucorreia2901@gmail.com>
2024-04-01 14:19:04 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,16 @@
+# Site Responsivo
+
+💻 Projeto
+O site foi desenvolvido buscando focar na responsividade.
+
+### Tecnologias
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+:white_check_mark: HTML5
+
+:white_check_mark: CSS
+
+:white_check_mark: JavaScript
+
+🔖 Layout
+Você pode visualizar o layout do projeto através desse <a href="https://alex-filipe.github.io/site-responsivo/" target="_blank">link.
---
+++ main.js
@@ -0,0 +1,76 @@
+const nav = document.querySelector('#header nav')
+
+/*Open and close menu*/
+const toggle = document.querySelectorAll('nav .toggle')
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+/*Close menu click */
+const links = document.querySelectorAll('nav ul li a')
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+/*Scroll */
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `
+#home .text, #home .image, 
+#about .image, 
+#services .header, #services .card,
+#contact .text, #contact .links,
+footer .brand, footer .social`,
+
+  { interval: 100 }
+)
+
+/*Button to top*/
+const backToTopButton = document.querySelector('.back-to-top')
+function backToTop() {
+  if (window.scrollY >= 500) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+/*Menu visible check*/
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+
+window.addEventListener('scroll', function () {
+  backToTop()
+  activateMenuAtCurrentSection()
+})
